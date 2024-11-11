04dd655405b0c88316da308158b9d7a90de51fe8
Enis Gayretli <enis@botanalytics.co>
2024-11-11 12:33:31 +0300

    Return statements are added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,22 +6,22 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        return false;
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+       return false;
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+     return false;
     }
 
     public static double area(double width, double height) {
-       
+       return 0.0;
     }
 
     public static double area(double radius) {
-       
+       return 0.0;
     }
 }
